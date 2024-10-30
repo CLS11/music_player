1b4fc9a5ab047e19fb98deece0d2533d3a6c7c27
CLS11 <crystall.101600@gmail.com>
2024-10-30 14:38:11 +0530

    Playlist navigator.

---
+++ lib/models/playlist_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/models/song.dart';
+
+class PlaylistProvider extends ChangeNotifier{
+  //PLAYLIST OF SONGS
+
+  final List<Song> _playlist = [
+    
+  ];
+}
