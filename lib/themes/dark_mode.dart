eccb1a857b16a614fa66998de67bd5e5c55f762a
CLS11 <crystall.101600@gmail.com>
2024-10-30 14:36:07 +0530

    Color scheme for the dark mode of the app.

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: const Color.fromARGB(255, 72, 69, 69),
+    primary: Colors.grey.shade900,
+    secondary: Colors.grey.shade800,
+    inversePrimary: Colors.grey.shade100,
+  ),
+);
