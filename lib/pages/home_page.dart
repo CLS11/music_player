34a4686bf12d5280fbcc6b7e4b0bd5b7e32a3b6a
CLS11 <crystall.101600@gmail.com>
2024-10-31 14:09:30 +0530

    Added a drawer.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:music_player/components/drawer.dart';
+import 'package:music_player/models/playlist_provider.dart';
+import 'package:music_player/models/song.dart';
+import 'package:music_player/pages/song_page.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +13,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //Fetch the playlist provider
+  late final dynamic playlistProvider;
+
+  @override
+  void initState() {
+    super.initState();
+
+    playlistProvider = Provider.of<PlaylistProvider>(context, listen: false);
+  }
+
+  void goToSong(int songIndex) {
+    playlistProvider.currentSongIndex = songIndex;
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SongPage(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +41,29 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("PLAYLIST"),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
+      body: Consumer<PlaylistProvider>(
+        builder: (context, value, child) {
+          //Fetching the playlist
+          final List<Song> playlist = value.playlist;
+
+          //Returns List View UI
+          return ListView.builder(
+            itemCount: playlist.length,
+            itemBuilder: (context, index) {
+              //Fetching individual songs
+              final Song song = playlist[index];
+              //Returns List Tile UI
+              return ListTile(
+                title: Text(song.songName),
+                subtitle: Text(song.artistName),
+                leading: Image.asset(song.albumArtImagePath),
+                onTap: () => goToSong(index),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
